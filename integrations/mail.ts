a0585f905506e394b117db0795368e14b30db7a5
Aayush Garg <aayushgarg2006@gmail.com>
2023-01-01 07:41:21 +0530

    mail

--- integrations/mail.ts
+++ integrations/mail.ts
@@ -1,31 +1,31 @@
 import nodemailer from 'nodemailer';
 
 export async function spamEmailWithFakeNewsletters(
-    email: string,
-    message: string,
-    subject: string,
-    loginUsername: string = "epoch_kids",
-    password: string = "okokokok"
-    ) {
-    // login to email using nodemailer with these credentials and then send the email message
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: loginUsername,
-            pass: password
-        }
-    });
-    const mailOptions = {
-        from: loginUsername,
-        to: email,
-        subject: subject,
-        text: message
-    };
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
+  email: string,
+  message: string,
+  subject: string,
+  loginUsername: string = process.env.MAIL!,
+  password: string = process.env.MAIL_PASSWORD!
+) {
+  // login to email using nodemailer with these credentials and then send the email message
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: loginUsername,
+      pass: password,
+    },
+  });
+  const mailOptions = {
+    from: loginUsername,
+    to: email,
+    subject: subject,
+    text: message,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 }
